04958297719c0eb23f8710d288b66d02e883e11b
Colleen Cummings <aiguru2002@gmail.com>
2026-05-13 21:56:24 +0000

    add terraform lab

---
+++ terraform/main.tf
@@ -0,0 +1,8 @@
+terraform {
+  required_version = ">= 1.6.0"
+}
+
+resource "local_file" "lab_summary" {
+  filename = "${path.module}/cloud_lab_summary.txt"
+  content  = "Cloud Lab includes Python, Docker, Jenkins, Prometheus, Grafana, Kubernetes, Helm, and Terraform."
+}
